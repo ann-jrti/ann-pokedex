4035365582e025efdcb56b73a9f6ee3c395e9ae1
ann <asog.jrti@gmail.com>
2022-02-19 15:46:26 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Router, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import NotFound from './pages/not-found';
+import PokeHeader from './components/poke-header';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
     <div className="App">
       <BrowserRouter>
+        {/* <PokeHeader></PokeHeader> */}
         <Routes>
           <Route path="/" element={<Home></Home>} ></Route>
           <Route path="*" element={<NotFound></NotFound>} ></Route>
