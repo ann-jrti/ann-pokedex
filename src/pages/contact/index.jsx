cbddd3dc1a93c1a77371716c51407ea8ce441a3c
ann <asog.jrti@gmail.com>
2022-02-21 09:35:02 +0100

    fix

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,51 +3,47 @@ import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function Contact() {
-    const formSubmitApi = 'https://formsubmit.co/ajax/andreamaso.sm@gmail.com';
-    const backHome = useNavigate();
-    const [response, updateResponse] = useState("")
-    const location= useLocation();
-    const poke = new URLSearchParams(useLocation().search);
-    const name = poke.get("name")
-    console.log(name);
+  const formSubmitApi = 'https://formsubmit.co/ajax/andreamaso.sm@gmail.com';
+  const backHome = useNavigate();
+  const [response, updateResponse] = useState("")
+
+  return (
+    <main>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        fetch(formSubmitApi, {
+          method: "POST"
+        })
+          .then(response => response.json())
+          .then(info => {
+            updateResponse("Thank you for your register!")
+            setTimeout(() => {
+              backHome('/home')
+            }, 3000)
+          })
+      }}>
+
+        <h2 className='form__register-title'>Register here</h2>
+        <div className="form__input-container">
+          <label className="form__label" htmlFor="name">Name</label>
+          <input className="form__input" name="name" type="text" placeholder="Your name" required></input>
+        </div>
+
+        <div className="form__input-container">
+          <label className="form__label" htmlFor="email">Email</label>
+          <input className="form__input" name="email" type="email" placeholder="Your Email" required></input>
+        </div>
 
 
-    return(
-        <main>
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            fetch(formSubmitApi, {
-              method: "POST",
-              body: new FormData(e.target)})
-              .then(response => response.json())
-              .then(info => {
-                updateResponse("Thank you for your register!")
-                  setTimeout(()=>{
-                    backHome('/home') 
-                  }, 3000)
-                })
-            }}>
+        {/* <textarea placeholder="Your Message" name="message" rows="5" required></textarea> */}
 
-            <h2 className='form__register-title'>Register here</h2>
-            <div className="form__input-container">
-              <label className="form__label" htmlFor="name">Name</label>
-              <input className="form__input" name="name" type="text" placeholder="Your name" required></input>
-            </div>
-            
-            <div className="form__input-container">
-              <label className="form__label" htmlFor="email">Email</label>
-              <input className="form__input" name="email" type="email" placeholder="Your Email" required></input>
-            </div>
-            
-            
-            {/* <textarea placeholder="Your Message" name="message" rows="5" required></textarea> */}
-
-            <button className="submit" type="submit">Submit</button>
-        </form>
+        <button className="submit" type="submit">Submit</button>
         <h2>{response}</h2>
-        </main>
-       
-    )
+      </form>
+
+    </main>
+
+  )
 }
 
 export default Contact;
