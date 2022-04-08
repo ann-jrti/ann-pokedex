9014280e72974cd31578a85f7f97cb6d4c9f6fd0
ann <asog.jrti@gmail.com>
2022-04-08 16:22:57 +0100

    remove contact section and styles home

--- src/components/poke-header/index.jsx
+++ src/components/poke-header/index.jsx
@@ -1,16 +1,16 @@
-import './style.css'
+import './style.css';
 import { Link } from 'react-router-dom';
 
 function PokeHeader() {
-
-    return (
-        <nav>
-            <div className="pokemon__header">
-                <Link className="pokemon__link-home" to="/home"><h1 className="pokemon__pokedex-title">Pokédex</h1></Link>
-                <Link className="pokemon__link-home" to="/contact"><h1 className="pokemon__pokedex-title">Contact</h1></Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="pokemon__header">
+        <Link className="pokemon__link-home" to="/home">
+          <h1 className="pokemon__pokedex-title">Ann's Pokédex</h1>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default PokeHeader;
--- src/pages/detail/components/detail-info/index.jsx
+++ src/pages/detail/components/detail-info/index.jsx
@@ -1,7 +1,7 @@
-import "./style.css";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import PokeHeader from "../../../../components/poke-header";
+import './style.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PokeHeader from '../../../../components/poke-header';
 
 function PokemonDetail({
   name,
@@ -14,71 +14,75 @@ function PokemonDetail({
   spDefense,
   speed,
 }) {
-
   const [detailImage, setDetailImage] = useState(null);
   console.log(detailImage);
   return (
     <React.Fragment>
-    <main className="pokemon__detail-container">
-      <h1 className="pokemon__details-text">Pokemon details</h1>
-      <article className="pokemon__detail">
-        <div class="pokeball__box">
-          <img
-            class="pokemon__pokeball-background"
-            src="https://pokemon-world-imdoug.netlify.app/img/pokeball.svg"
-          />
+      <main className="pokemon__detail-container">
+        <div className="two">
+          <Link to="/home">
+            <div className="pokemon__back-home-container">
+              <button className="pokemon__details-back-home">
+                <span className="arrow">←</span> Back to pokemon list
+              </button>
+            </div>
+          </Link>
         </div>
-        <div className="pokemon__detail-img-container ">
-          <img
-            src={detailImage ? detailImage : img}
-            className="imagen bounce pokemon__detail__image"
-          ></img>
-         
-        </div>
-        <button
-            className="pokemon__version"
-            onClick={() => {
-              setDetailImage(detailImage ? null : imgReverse);
-            }}
-          >
-            {detailImage ? "normal version" : "shinny version"}
-          </button>
-        <h2 className="pokemon__detail__name">{name}</h2>
-        <section className="pokemon__stats-container">
-          <div className="pokemon__stats-text-container">
-            <h2 className="pokemon__stats-text">Stats</h2>
-          </div>
 
-          <div className="pokemon__stats-value-container">
-            <p className="pokemon__stat">
-              Hp:<span>{hp}</span>
-            </p>
-            <p className="pokemon__stat">
-              Attack: <span>{attack}</span>
-            </p>
-            <p className="pokemon__stat">
-              Defense: <span>{defense}</span>
-            </p>
-            <p className="pokemon__stat">
-              Special attack: <span>{spAttack}</span>
-            </p>
-            <p className="pokemon__stat">
-              Special defense: <span>{spDefense}</span>
-            </p>
-            <p className="pokemon__stat">
-              Speed: <span>{speed}</span>
-            </p>
-          </div>
-          {/* <p className="">Height | <span>{height} cm</span></p>  */}
-        </section>
-        <Link to="/home">
-        <button className="pokemon__details-back-home"><span className="arrow">←</span> Back to pokemon list</button>
-        </Link> 
-      </article>
-    </main>
-   
+        <div class="one">
+          <article className="pokemon__detail">
+            <div class="pokeball__box">
+              <img
+                class="pokemon__pokeball-background"
+                src="https://pokemon-world-imdoug.netlify.app/img/pokeball.svg"
+              />
+            </div>
+            <div className="pokemon__detail-img-container ">
+              <img
+                src={detailImage ? detailImage : img}
+                className="imagen bounce pokemon__detail__image"
+              ></img>
+            </div>
+            <button
+              className="pokemon__version"
+              onClick={() => {
+                setDetailImage(detailImage ? null : imgReverse);
+              }}
+            >
+              {detailImage ? 'switch normal version' : 'switch shinny version'}
+            </button>
+            <h2 className="pokemon__detail__name">{name}</h2>
+            <section className="pokemon__stats-container">
+              <div className="pokemon__stats-text-container">
+                <h2 className="pokemon__stats-text">Stats</h2>
+              </div>
+
+              <div className="pokemon__stats-value-container">
+                <p className="pokemon__stat">
+                  Hp:<span>{hp}</span>
+                </p>
+                <p className="pokemon__stat">
+                  Attack: <span>{attack}</span>
+                </p>
+                <p className="pokemon__stat">
+                  Defense: <span>{defense}</span>
+                </p>
+                <p className="pokemon__stat">
+                  Special attack: <span>{spAttack}</span>
+                </p>
+                <p className="pokemon__stat">
+                  Special defense: <span>{spDefense}</span>
+                </p>
+                <p className="pokemon__stat">
+                  Speed: <span>{speed}</span>
+                </p>
+              </div>
+              {/* <p className="">Height | <span>{height} cm</span></p>  */}
+            </section>
+          </article>
+        </div>
+      </main>
     </React.Fragment>
-
   );
 }
 
